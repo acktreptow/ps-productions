67abbb8adcf62e3d5aba231edbaefa0579bc40be
acktreptow <acktreptow@gmail.com>
2024-03-17 06:32:36 +1400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="pt-5 pb-4 mt-5">
       <p className="text-light text-center">
-        Copyright &copy; PlayStation Productions 2023
+        Copyright &copy; PlayStation Productions 2024
       </p>
     </footer>
   );
